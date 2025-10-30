e6cdc4d5f07fd4076e29f196f9f4a89209dd89ad
dcreadon <115508514+dcreadon@users.noreply.github.com>
2025-10-30 04:16:20 -0500

    added comments to server.c and created some of README.md file

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# cp-2_CYBE4400
-Class Project 2 for Operating System Security Course
+# cp-2_CYBE4400: Class Project 2 for Operating System Security Course
+---
+
+## Building the Environment
+- In order to be able to run this tutorial the correct environemtn will need to be set up using QEMU. The provided document below gives a step by step explanation of how to set up this environment. 
+- Refer to Setting up seL4 on QEMU-2 document that is provided in this github
+
+## Cloning the Repo
+- Once you have the environment set up correctly the next step is to repo the repository.
+- In the home folder, run mkdir sel4_tutorials_manifest, and then cd into this folder
+- From here run: repo init -u https://github.com/seL4/sel4-tutorials-manifest and then repo sync
+- Once sync is done, you have successfully cloned the necessary repo for this project.
+
+## Project Structure
+- For this project the specific tutorial we worked with was the ipc tutorial
+- To get this tutorial, run ./init --tut ipc from within the seL4_tutorial_manifest folder
+- This creates the necessary ipc and ipc_build folders
+- ipc contains the source code of the project (client1.c, client2.c, CMakeLists.txt and server.c)
+- ipc_build is where the project will be compiled and ran/tested
+
+## Running the Project
+- As stated earlier, /seL4_tutorials_manifest/ipc contains all of the source code necessary for this project
+- To run this project, simply download this reposity and replace the ipc folder you currently have, with the one from this repo
+- Follwing this navigate to the ipc_build folder
+- From this folder, run ninja to compile the source code you juse placed into ipc
+- Once the ninja build is complete, run the tutorial with ./simulate and observe the output
+
+## Extra Notes
+- To run each of the tasks some things will need to be commented out.
+- For task 1, all of the else statement can be commented out.
+- For task 2, all of the else statement other than the len and for loop can be commented out.
+- For task 3, all of the else statemnt other than len variable, for loop, and seL4_ReplyRecv can be commented out
+- For task 4, run as is.
--- server.c
+++ server.c
@@ -29,13 +29,16 @@ int main(int c, char *argv[]) {
                                                 seL4_AllRights, badge);
              printf("Badged %lu\n", badge);
 
-             // SetCap is used to set the extraCap to be sent to the client
-             
-	     seL4_SetCap(0, free_slot);
-	     seL4_MessageInfo_t reply_to_client = seL4_MessageInfo_new(0, 0, 1, 0);
+             /* Here SetCap is being used to set the current badge capability of the connected client 
+                free_slot will hold this capability which will then be sent back to the client. a reply_to_client 
+                variable is declared which will be the reply sent to the client. This reply uses the messageinfo_new 
+                function and specifies the third parameter which means that an extraCap will be sent */
+
+	        seL4_SetCap(0, free_slot); 
+	        seL4_MessageInfo_t reply_to_client = seL4_MessageInfo_new(0, 0, 1, 0);
 	     
              /* reply to the sender and wait for the next message */
-             // replace info with reply_to_client so we can send our response back
+             //replace info with reply_to_client so we can send our response back
              seL4_Reply(reply_to_client);
 
              /* now delete the transferred cap */
@@ -48,7 +51,11 @@ int main(int c, char *argv[]) {
 
              // TODO use printf to print out the message sent by the client
              // followed by a new line
-             
+             /* error variable is used here to hold the information from the first connected client. It utilized the free_slot
+                hold the "reply capability" of the connected client. Next, a len variable is established to get the length of the current 
+                message the client is trying to send which then gets fed into a for loop to print the current message of the connected client
+                following that a seL4_Recv function is called to get the other client connected. Once this is done, a reply is sent to the first connected
+                client so the second client can then send its message. This process then repeats until the clients are out of messages */
              error = seL4_CNode_SaveCaller(cnode, free_slot, seL4_WordBits);
              assert(error == 0);
              
@@ -64,8 +71,13 @@ int main(int c, char *argv[]) {
              seL4_Send(free_slot, seL4_MessageInfo_new(0, 0, 0, 0));
 
 
-	      //TODO
-             //Function necessary to complete task 3 but not needed for Task 4
+	      
+             /* Function necessary to complete task 3 but not needed for Task 4.
+                Simply replies and receives a message in the same function.
+                good for instantaneous response from the client.
+                lets each client communicate before the other can.
+                i.e clients communicate all messages in a row. */
+
              //info = seL4_ReplyRecv(endpoint, info, &sender);
         }
     }
