115c1b1b56e7ac2e656229d208fb0802e1e50d7c
dcreadon <115508514+dcreadon@users.noreply.github.com>
2025-10-30 03:45:04 -0500

    Add files via upload
    Adding finished and necessary files for project turn in

---
+++ server.c
@@ -0,0 +1,74 @@
+
+
+#include <assert.h>
+#include <sel4/sel4.h>
+#include <stdio.h>
+#include <utils/util.h>
+
+// cslot containing IPC endpoint capability
+extern seL4_CPtr endpoint;
+// cslot containing a capability to the cnode of the server
+extern seL4_CPtr cnode;
+// empty cslot
+extern seL4_CPtr free_slot;
+
+int main(int c, char *argv[]) {
+
+	seL4_Word sender;
+    seL4_MessageInfo_t info = seL4_Recv(endpoint, &sender);
+    while (1) {
+	    seL4_Error error;
+        if (sender == 0) {
+
+             /* No badge! give this sender a badged copy of the endpoint */
+             seL4_Word badge = seL4_GetMR(0);
+             
+             // a copy of the shared enpoint is minted into free_slot to be sent
+             seL4_Error error = seL4_CNode_Mint(cnode, free_slot, seL4_WordBits,
+                                                cnode, endpoint, seL4_WordBits,
+                                                seL4_AllRights, badge);
+             printf("Badged %lu\n", badge);
+
+             // SetCap is used to set the extraCap to be sent to the client
+             
+	     seL4_SetCap(0, free_slot);
+	     seL4_MessageInfo_t reply_to_client = seL4_MessageInfo_new(0, 0, 1, 0);
+	     
+             /* reply to the sender and wait for the next message */
+             // replace info with reply_to_client so we can send our response back
+             seL4_Reply(reply_to_client);
+
+             /* now delete the transferred cap */
+             error = seL4_CNode_Delete(cnode, free_slot, seL4_WordBits);
+             assert(error == seL4_NoError);
+
+             /* wait for the next message */
+             info = seL4_Recv(endpoint, &sender);
+        } else {
+
+             // TODO use printf to print out the message sent by the client
+             // followed by a new line
+             
+             error = seL4_CNode_SaveCaller(cnode, free_slot, seL4_WordBits);
+             assert(error == 0);
+             
+             
+             int len = seL4_MessageInfo_get_length(info);
+             for (int i = 0; i < len; i++){
+             	printf("%c", (char) seL4_GetMR(i));
+             }
+             printf("\n");
+             
+             info = seL4_Recv(endpoint, &sender);
+             
+             seL4_Send(free_slot, seL4_MessageInfo_new(0, 0, 0, 0));
+
+
+	      //TODO
+             //Function necessary to complete task 3 but not needed for Task 4
+             //info = seL4_ReplyRecv(endpoint, info, &sender);
+        }
+    }
+
+    return 0;
+}
